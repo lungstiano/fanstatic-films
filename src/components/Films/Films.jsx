46ed344308c21c4ae02579888b58cc459fed1b96
Lungelo <dlaminilungelo17@gmail.com>
2022-03-06 15:52:10 +0200

    Create some components

---
+++ src/components/Films/Films.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./films.css";
+
+const Films = () =>{
+    return(
+        <div>
+
+            movie component
+
+        </div>
+                
+    
+    
+    );
+
+}
+
+
+
+export default Films;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () =>{
+    return(
+        <div className="navBar">
+                <ul className="navItems">
+                    <li> <a href="#">Fanstastic Films</a> </li>
+                    <li> <a href="#">Home</a> </li>
+                    <li> <a href="#">Popular Films</a> </li>
+                    <li> <a href="#">About the developer</a> </li>
+                </ul>
+        </div>
+    
+    
+    );
+
+}
+
+
+
+export default Navigation;
