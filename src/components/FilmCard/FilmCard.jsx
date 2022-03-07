2cb21099d4f0b78dd43b432076cfd9498f08f5e3
Lungelo <dlaminilungelo17@gmail.com>
2022-03-07 17:00:13 +0200

    display films

--- src/components/FilmCard/FilmCard.jsx
+++ src/components/FilmCard/FilmCard.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const FilmCard = ({film}) =>{
+    const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
+    console.log(film)
 
     return(
-        <div>
-            {film.title}
+        <div className="film-card">
+            {film.poster_path ? <img className={"film-cover"} src={`${IMAGE_PATH}${film.poster_path}`} alt=""/>
+             : 
+             <div className={"film-placeholder"}>
+                 No Image Found
+             </div>
+            }
+
+            <h5 className={"film-title"}>{film.title}</h5>
+            
     
         </div>
     )
